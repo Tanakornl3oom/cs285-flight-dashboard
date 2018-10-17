351853a39096f95f8e1882b9fdcbc45feac67168
tanakornl3oom <thanakorn_l3oomlnw@hotmail.com>
2018-10-17 19:22:01 +0700

    add css

--- public/flight_list.js
+++ public/flight_list.js
@@ -11,73 +11,73 @@ let currentPageSelection = 1;
 let maximumPageSelection = 1;
 
 function renderFlightsRow(flights) {
-	flightsTable.html("");
+  flightsTable.html("");
 
-	flights.forEach(flight => {
-		const tableData = `<tr>
+  flights.forEach(flight => {
+    const tableData = `<tr>
 			<td>${flight.price}</td>
 			<td>${flight.airLine}</td>
 			<td>${flight.takeOff}</td>
 			<td>${flight.landing}</td>
 			<td>${flight.stop}</td>
 		</tr>`;
-		flightsTable.append(tableData);
-	});
+    flightsTable.append(tableData);
+  });
 }
 
 function prevPagination() {
-	if (currentPageSelection > 1) fetchFlights(--currentPageSelection);
+  if (currentPageSelection > 1) fetchFlights(--currentPageSelection);
 }
 function nextPagination() {
-	if (currentPageSelection < maximumPageSelection)
-		fetchFlights(++currentPageSelection);
+  if (currentPageSelection < maximumPageSelection)
+    fetchFlights(++currentPageSelection);
 }
 function renderFlightsPagination(flightsPageTotal) {
-	flightsPagination.html("");
+  flightsPagination.html("");
 
-	flightsPagination.append(
-		`<li class="page-item">
+  flightsPagination.append(
+    `<li class="page-item">
 			<a class="page-link" href="#" onclick="prevPagination()">Previous</a>
 		</li>`
-	);
-	for (let i = 0; i < flightsPageTotal; i++) {
-		flightsPagination.append(
-			`<li class="page-item"><a id="page-link-${i +
-				1}"class="page-link" href="#" onclick="fetchFlights(${i + 1})">${i +
-				1}</a></li>`
-		);
-	}
-	flightsPagination.append(
-		`<li class="page-item">
+  );
+  for (let i = 0; i < flightsPageTotal; i++) {
+    flightsPagination.append(
+      `<li class="page-item"><a id="page-link-${i +
+        1}"class="page-link" href="#" onclick="fetchFlights(${i + 1})">${i +
+        1}</a></li>`
+    );
+  }
+  flightsPagination.append(
+    `<li class="page-item">
 			<a class="page-link" href="#" onclick="nextPagination()">Next</a>
 		</li>`
-	);
+  );
 }
 
 function fetchFlights(pageNumber = 1, itemsPerPage) {
-	let query;
-	if (!pageNumber && !itemsPerPage) query = "";
-	else if (pageNumber && itemsPerPage)
-		query = `pageNumber=${pageNumber}&itemsPerPage=${itemsPerPage}`;
-	else if (pageNumber) query = `pageNumber=${pageNumber}`;
-	else if (itemsPerPage) query = `itemsPerPage=${itemsPerPage}`;
+  let query;
+  if (!pageNumber && !itemsPerPage) query = "";
+  else if (pageNumber && itemsPerPage)
+    query = `pageNumber=${pageNumber}&itemsPerPage=${itemsPerPage}`;
+  else if (pageNumber) query = `pageNumber=${pageNumber}`;
+  else if (itemsPerPage) query = `itemsPerPage=${itemsPerPage}`;
 
-	$.get(`/flights?${query}`, data => {
-		maximumPageSelection = data.flightsPageTotal;
+  $.get(`/flights?${query}`, data => {
+    maximumPageSelection = data.flightsPageTotal;
 
-		renderFlightsRow(data.flights);
-		//Header table navigation
-		flightsBadge.html(data.pageNumber);
-		flightsPageTotal.html(data.flightsPageTotal);
+    renderFlightsRow(data.flights);
+    //Header table navigation
+    flightsBadge.html(data.pageNumber);
+    flightsPageTotal.html(data.flightsPageTotal);
 
-		//Bottom table navigation
-		renderFlightsPagination(data.flightsPageTotal);
-		$(`#page-link-${currentPageSelection}`).removeClass("active");
-		$(`#page-link-${pageNumber}`).addClass("active");
-		currentPageSelection = pageNumber;
-	});
+    //Bottom table navigation
+    renderFlightsPagination(data.flightsPageTotal);
+    $(`#page-link-${currentPageSelection}`).removeClass("active");
+    $(`#page-link-${pageNumber}`).addClass("active");
+    currentPageSelection = pageNumber;
+  });
 }
 
 $(document).ready(() => {
-	fetchFlights();
+  fetchFlights();
 });
